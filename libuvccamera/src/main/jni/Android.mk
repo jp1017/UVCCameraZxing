76583bdd47d1798096b9e16636f3b8966796447c
jp1017 <jp8917@163.com>
2016-09-16 11:20:39 +0800

    delete C files update travis

--- libuvccamera/src/main/jni/Android.mk
+++
@@ -1,7 +0,0 @@
-#include $(call all-subdir-makefiles)
-PROJ_PATH	:= $(call my-dir)
-include $(CLEAR_VARS)
-include $(PROJ_PATH)/UVCCamera/Android.mk
-include $(PROJ_PATH)/libjpeg-turbo-1.5.0/Android.mk
-include $(PROJ_PATH)/libusb/android/jni/Android.mk
-include $(PROJ_PATH)/libuvc/android/jni/Android.mk
